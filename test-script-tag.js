b70e0b3ebfe396323b6af7db858ece92bdc98aff
Minh Quang Tran <minh.tranquang98@gmail.com>
2020-10-18 17:57:13 +0700

    Update test-script-tag.js

--- test-script-tag.js
+++ test-script-tag.js
@@ -1,1 +1,22 @@
 
+if (typeof omgage_checkJS == 'undefine') {
+  var omgage_checkJS = 1;
+    var omg_age_verifier_shopName = Shopify.shop;
+    var rootlinkAgeVerifier = "https://apps.omegatheme.com/age-verifier";
+
+    if (typeof $ == "undefined") {
+        javascript: (function (e, s) {
+            e.src = s;
+            e.onload = function () {
+                $ = jQuery.noConflict();
+                //omgage_init();
+            };
+            document.head.appendChild(e);
+        })(
+            document.createElement("script"),
+            "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"
+        );
+    } else {
+        //omgage_init();
+    }
+}
